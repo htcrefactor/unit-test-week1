3455f4608b27f651c485bea15f7c8ef36d1aa57e
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-25 19:07:22 +0900

    Initial workout state.

--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -14,16 +14,16 @@ public class UpperCaseCounterTest {
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_null_input(){
         String str = null;
-
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertThat(numberOfUpperCaseCharactersInString, is(0));
     }
 
     //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
-
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertThat(numberOfUpperCaseCharactersInString, is(0));
     }
 
     //대문자들이 포함된 문자열을 전달했을 때 카운팅된 숫자와 맞는지 검증하는 테스트 작성
@@ -34,8 +34,11 @@ public class UpperCaseCounterTest {
         int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
 
         //assertTrue로 맞는 테스트 코드 작성
+        assertTrue(numberOfUpperCaseCharactersInString == 10);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertFalse(numberOfUpperCaseCharactersInString == 1);
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        assertThat(numberOfUpperCaseCharactersInString, is(10));
     }
 
     //대소문자가 섞여 있을 때 정확히 카운팅 되는 지에 대한 테스트 코드 작성
@@ -46,6 +49,7 @@ public class UpperCaseCounterTest {
         //assertTrue로 맞는 테스트 코드 작성
         //assertFalse로 틀리는 값을 넣어 테스트 작성
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        assertThat(result, is(6));
         System.out.println("result :: " + result);
     }
 
